760a7a76595b74cf4797294dd16fe33d14f5f414
FeDemir <dmrfrht@gmail.com>
2021-09-26 14:47:29 -0500

    2nd attemp

---
+++ src/Loop.java
@@ -0,0 +1,16 @@
+public class Loop {
+    public static void main(String[] args) {
+        int count=0;
+        while(++count<=100){
+            System.out.print(count+"\t");
+        }
+        System.out.println(count);
+        int[] myNum=new int[]{};
+        count=0;
+        while(count<=255){
+            System.out.print((char)count+++"");
+
+
+        }
+        System.out.println( );
+    }}
