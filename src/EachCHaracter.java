fa6f5b81a059273be71be0f35f78be4ae00ca5e6
FeDemir <dmrfrht@gmail.com>
2021-09-26 14:45:39 -0500

    1st attemp

---
+++ src/EachCHaracter.java
@@ -0,0 +1,14 @@
+public class EachCHaracter {
+    public static void main(String[] args) {
+        String java="java";
+        //           0123
+        System.out.println(java.charAt(0));
+        System.out.println(java.charAt(1));
+        System.out.println(java.charAt(2));
+        System.out.println(java.charAt(3));
+        System.out.println(java.substring(0,2));
+        char a=java.charAt(0);
+        //char b=java.substring(0,1);
+        System.out.println(""+java.charAt(0)+java.charAt(1));
+    }
+}
